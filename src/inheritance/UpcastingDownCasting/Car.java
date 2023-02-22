244589ed14126bc4b9fdf271c15404f5b1eaf7fd
Nigmat <harrynick005@gmail.com>
2023-02-21 23:32:17 -0500

    Upcasting And DownCasting

---
+++ src/inheritance/UpcastingDownCasting/Car.java
@@ -0,0 +1,27 @@
+package inheritance.UpcastingDownCasting;
+
+public class Car {
+
+    public void ignition(){
+        System.out.println("Engine started");
+    }
+
+    public void forward(){
+        System.out.println("car is moving forward");
+    }
+
+    public void reverse(){
+        System.out.println("car is going backward");
+    }
+
+    public void horn(){
+        System.out.println("Bebebe");
+    }
+    public void brake(){
+        System.out.println("car stops");
+    }
+
+    public void park(){
+        System.out.println("Engine stall");
+    }
+}
