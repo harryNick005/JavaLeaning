e6ccc20ac4bee3aa3d7914f93fb5f0afc71c55a3
Nigmat <harrynick005@gmail.com>
2023-03-01 22:18:03 -0500

    String buffer and builder

--- src/InterviewPractice/stringReverse.java
+++ src/InterviewPractice/stringReverse.java
@@ -4,7 +4,8 @@ public class stringReverse {
     // how to reverse a string "Hello World"  ===> dlorW olleH
     // write a method to reverse String
      String  result="";  // 1.global 2. instance
-    public static String reverseString(String input){
+    // 1
+    public static String reverse_String(String input){
         String result="";
         for(int i=input.length()-1;i>=0;i--){
 
@@ -15,9 +16,30 @@ public class stringReverse {
 
     }
 
+
+
+
+    //2 StringBuffer StringBuilder
+
+    public static String StringBuffer_Reverse(String input){
+        StringBuffer stringBuffer=new StringBuffer(input);
+        return stringBuffer.reverse().toString();
+    }
+
+    public static String StringBuilder_Reverse(String input){
+        StringBuilder stringBuilder=new StringBuilder(input);
+        return stringBuilder.reverse().toString();
+    }
+
+
     public static void main(String[] args) {
-        String result=stringReverse.reverseString("Hello World");
+        String result=stringReverse.reverse_String("Hello World");
         System.out.println(result);
+
+        System.out.println(stringReverse.StringBuffer_Reverse("Hellow Java"));
+        System.out.println(stringReverse.StringBuilder_Reverse("Hellow Python"));
+
+
 
     }
 }
